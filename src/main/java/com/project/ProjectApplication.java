7759b5e9111592a7af3490adb3c6661e76fae4b6
adelin232 <adelin_extreme_08@yahoo.com>
2022-04-07 23:45:00 +0300

    Create project using spring initializr && Add auth0 for 3rd party user management

---
+++ src/main/java/com/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+}
