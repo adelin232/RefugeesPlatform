4344ad8eb8f090a237b88853dbaa9f72b5e2b145
alexvamesu <alex.vamesu@yahoo.ro>
2022-04-10 11:32:03 -0400

    Fix reverse proxy

--- script.sh
+++ script.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+
+# stop running containers
 docker-compose down
 echo ""
 sleep 1
+
+# remove database files
 sudo rm -rf ./mysql/mysql-data/*
+
+# start new containers
 docker-compose up --force-recreate --detach --build
 echo ""
 sleep 1
+
+# add hostnames to /etc/hosts
+
+for _file in /etc/hosts; do
+    if ! grep -qF '127.0.0.1        local.proxy.spring' $_file; then
+        sudo echo "127.0.0.1        local.proxy.spring" >> $_file
+    fi
+    if ! grep -qF '127.0.0.1        local.proxy.adminer' $_file; then
+        sudo echo "127.0.0.1        local.proxy.adminer" >> $_file
+    fi
+    if ! grep -qF '127.0.0.1        local.proxy.portainer' $_file; then
+        sudo echo "127.0.0.1        local.proxy.portainer" >> $_file
+    fi
+done
+
+# show running containers
 docker ps
