8cfdcd9e363ed463889e5b74142ec73730cfa2b8
adelin232 <adelin_extreme_08@yahoo.com>
2022-04-15 00:44:28 +0300

    Split general unit tests class

---
+++ project/src/test/java/com/project/UserTests.java
@@ -0,0 +1,29 @@
+package com.project;
+
+import com.project.models.User;
+import com.project.repositories.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class UserTests {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("Insert user should work")
+    void insertUserTest() {
+        User user = new User(1L, "test@test.com", "0747553042", "Narcis-Adelin", "Miulet", "Narcis-Adelin Miulet");
+
+        userRepository.save(user);
+        assertThat(userRepository.findById(user.getId())).hasValue(user);
+    }
+}
