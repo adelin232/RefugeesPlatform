25f24264028fe9f614e2b08a2dcc20c2ae85671c
adelin232 <adelin_extreme_08@yahoo.com>
2022-05-17 14:10:24 +0300

    Repair script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,7 @@
 docker-compose down --remove-orphans
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
 rm -r ./mysql/mysql-data/*
-rm -rf ./grafana/grafana-storage/*
+rm -r ./grafana/grafana-storage/*
 docker-compose up --force-recreate --detach --build
 echo ""
 sleep 1
