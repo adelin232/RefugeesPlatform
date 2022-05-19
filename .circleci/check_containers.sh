41c8bbc1574b52c428dd9f0fddbbbf05bd197bc6
Alex Vamesu <57949787+alexvamesu@users.noreply.github.com>
2022-05-19 13:27:26 -0400

    Fix CircleCI errors

--- .circleci/check_containers.sh
+++ .circleci/check_containers.sh
@@ -7,26 +7,17 @@ GREEN=$(tput setaf 2)
 
 # Get container names from docker-compose
 mapfile -t container_names < <(cat docker-compose.yml | grep "container_name" | cut -d ":" -f2 | tr -d " ")
-for i in "${!container_names[@]}";
-do
-    container_names[$i]=${container_names[$i]::-1}
-done
 
 # Get proxy
 mapfile -t proxy_names < <(cat .env | grep "PROXY_ADDRESS" | cut -d "_" -f1)
 mapfile -t proxy_address < <(cat .env | grep "PROXY_ADDRESS" | cut -d "=" -f2)
-for i in "${!proxy_address[@]}";
-do
-    proxy_address[$i]=${proxy_address[$i]::-1}
-done
 
 # Get env variables
 MYSQLDB_USER=$(cat .env | grep "MYSQLDB_USER" | cut -d "=" -f2 | tr -d "\n")
-MYSQLDB_ROOT_PASSWORD=$(cat .env | grep "MYSQLDB_ROOT_PASSWORD" | cut -d "=" -f2 | tr -d "\n")
+MYSQLDB_ROOT_PASSWORD=$(cat .env | grep "MYSQLDB_ROOT_PASSWORD" | cut -d "=" -f2 | tr -d " \n")
 
-SPRING_RABBITMQ_USERNAME=$(cat .env | grep "GRAFANA_USER" | cut -d "=" -f2 | tr -d "\n")
-SPRING_RABBITMQ_PASSWORD=$(cat .env | grep "GRAFANA_PASSWORD" | cut -d "=" -f2 | tr -d "\n")
-
+SPRING_RABBITMQ_USERNAME=$(cat .env | grep "SPRING_RABBITMQ_USERNAME" | cut -d "=" -f2 | tr -d "\n")
+SPRING_RABBITMQ_PASSWORD=$(cat .env | grep "SPRING_RABBITMQ_PASSWORD" | cut -d "=" -f2 | tr -d "\n")
 
 echo "Check containers status:"
 # Iterate through array
@@ -38,9 +29,9 @@ do
         printf "%-40s%s\n" "--> [$name]" "[${GREEN}✔${NORMAL} ]"
 
         # Get container status
-    STATUS=$(docker inspect --format="{{.State.Status}}" $name | tr '[a-z]' '[A-Z]')
+        STATUS=$(docker inspect --format="{{.State.Status}}" $name | tr '[a-z]' '[A-Z]')
     
-    if [ $STATUS == "RUNNING" ]
+        if [ $STATUS == "RUNNING" ]
         then
         # Show status
         printf "%-40s %s\n" "      - Status: $STATUS" "${GREEN}✓${NORMAL}"
@@ -93,14 +84,14 @@ do
         if [ "$name" == "nginx-proxy" ]
         then
             for i in "${!proxy_names[@]}";
-                do
-                    if [ $(curl -k -I "https://${proxy_address[$i]}" 2>&1 | grep "server: nginx" | wc -l) != 0 ]
+            do
+                if [ $(curl -k -I "https://${proxy_address[$i]}" 2>&1 | grep "server: nginx" | wc -l) != 0 ]
                 then
                     printf "%-40s %s\n" "      - ${proxy_names[$i]^} proxy is up" "${GREEN}✓${NORMAL}"
                 else
                     printf "%-40s %s\n" "      - ${proxy_names[$i]^} proxy is up" "${RED}x${NORMAL}"
                 fi
-                done
+            done
         fi
 
         # Check rabbitmq-broker
@@ -123,11 +114,11 @@ do
         # Check rabbitmq-listener
         if [ "$name" == "rabbitmq-listener" ]
         then
-            if [ $(curl -u $SPRING_RABBITMQ_USERNAME:$SPRING_RABBITMQ_PASSWORD http://localhost:15672/api/connections 2>&1 | grep -Po '"connection_name":(\d*?,|.*?[^\\]",)' | cut -d ":" -f2 | tr -d \",) != "Python_pika_connection" ]
+            if (( $(curl -u $SPRING_RABBITMQ_USERNAME:$SPRING_RABBITMQ_PASSWORD http://localhost:15672/api/connections 2>&1 | grep "Python_pika_connection" | wc -l) != 0 ))
             then
-                printf "%-40s %s\n" "      - Rabbitmq connection" "${RED}x${NORMAL}"
-            else
                 printf "%-40s %s\n" "      - Rabbitmq connection" "${GREEN}✓${NORMAL}"
+            else
+                printf "%-40s %s\n" "      - Rabbitmq connection" "${RED}x${NORMAL}"
             fi
         fi
 
@@ -141,9 +132,9 @@ do
                 printf "%-40s %s\n" "      - Spring website is up" "${GREEN}✓${NORMAL}"
             fi
         fi
-    else
-        printf "%-40s %s\n" "      - Status: $STATUS" "${RED}x${NORMAL}"
-    fi
+        else
+            printf "%-40s %s\n" "      - Status: $STATUS" "${RED}x${NORMAL}"
+        fi
     else
         printf "%-40s%s\n" "--> [$name]" "(${RED}✖${NORMAL} )"
     fi
