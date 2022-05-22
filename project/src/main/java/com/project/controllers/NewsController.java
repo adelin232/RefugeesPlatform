5d0e3b2af56f72b300bd81dedf1f0d138888ad08
adelin232 <adelin_extreme_08@yahoo.com>
2022-05-22 14:05:52 +0300

    Update News to show "Last updated ..."

--- project/src/main/java/com/project/controllers/NewsController.java
+++ project/src/main/java/com/project/controllers/NewsController.java
@@ -13,9 +13,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Controller for the news page.
@@ -57,6 +61,63 @@ public class NewsController {
         }
 
         List<News> news_list = newsService.getNews("ukraine");
+        news_list = news_list.stream().sorted(Comparator.comparing(News::getPublish_time).reversed())
+                .collect(Collectors.toList());
+
+        for (News news : news_list) {
+            TimeZone tz = TimeZone.getTimeZone("UTC+3");
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            df.setTimeZone(tz);
+            String nowAsISO = df.format(new Date());
+            String publishTime = news.getPublish_time().substring(0, 18) + "Z";
+            Date d1, d2;
+
+            try {
+                d1 = df.parse(nowAsISO);
+                d2 = df.parse(publishTime);
+                long differenceInTime = d1.getTime() - d2.getTime();
+
+                long differenceInSeconds
+                        = TimeUnit.MILLISECONDS
+                        .toSeconds(differenceInTime)
+                        % 60;
+
+                long differenceInMinutes
+                        = TimeUnit
+                        .MILLISECONDS
+                        .toMinutes(differenceInTime)
+                        % 60;
+
+                long differenceInHours
+                        = TimeUnit
+                        .MILLISECONDS
+                        .toHours(differenceInTime)
+                        % 24;
+
+                long difference_In_Days
+                        = TimeUnit
+                        .MILLISECONDS
+                        .toDays(differenceInTime)
+                        % 365;
+
+                if (difference_In_Days != 0) {
+                    news.setPublish_time("Last updated " + difference_In_Days + " days, " +
+                            differenceInHours + " hours, " +
+                            differenceInMinutes + " minutes, " +
+                            differenceInSeconds + " seconds ago.");
+                } else if (differenceInHours != 0) {
+                    news.setPublish_time("Last updated " + differenceInHours + " hours, " +
+                            differenceInMinutes + " minutes, " +
+                            differenceInSeconds + " seconds ago.");
+                } else {
+                    news.setPublish_time("Last updated " + differenceInMinutes + " minutes, " +
+                            differenceInSeconds + " seconds ago.");
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
         model.addAttribute("news_list", news_list);
 
         return "news";
